d42c993982099d9ee247cde4cf8747ff72bf9f60
DanielMevs <dannymevs@hotmail.com>
2022-12-05 17:14:58 -0600

    got rid of extra folders

--- sqe_pytest_suite/confest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture()
-def driver():
-    driver = webdriver.Chrome(executable_path='../drivers/chromedriver.exe')
-
-    yield driver
-    driver.quit()
